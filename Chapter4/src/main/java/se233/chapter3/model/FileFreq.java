66d9ca990763154c4ed00feaa468afe65b570a81
Phutawa <phutawanmuangma@gmail.com>
2025-07-31 09:53:06 +0700

    Add files via upload

---
+++ Chapter4/src/main/java/se233/chapter3/model/FileFreq.java
@@ -0,0 +1,44 @@
+package se233.chapter3.model;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class FileFreq {
+    private String name;
+    private String path;
+    private Integer freq;
+    public FileFreq(String name, String path, Integer freq) {
+        this.name = name;
+        this.path = path;
+        this.freq = freq;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Integer getFreq() {
+        return freq;
+    }
+
+    public void setFreq(Integer freq) {
+        this.freq = freq;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{%s:%d}", name, freq);
+    }
+}
